239cfa7119aae7d8af1c8e90181d7ba327a7811e
jacklam718 <jacklam718@gmail.com>
2019-04-17 22:50:56 +0800

    Update total hamming distance

--- total_hamming_distance_477/v1.js
+++ total_hamming_distance_477/v1.js
@@ -3,31 +3,31 @@
  * @return {number}
  */
 var totalHammingDistance = function(nums) {  
-  const padding = dec2Binary(Math.max(...nums)).length;
   let totalDistance = 0;
   for (let i = 0; i < nums.length; i++) { 
-    for (let y = i+1; y < nums.length; y++) {
-      const d1 = dec2Binary(nums[i], padding);
-      const d2 = dec2Binary(nums[y], padding);
-      totalDistance += hammingDistance(d1, d2);
+    for (let j = i+1; j < nums.length; j++) {
+      totalDistance += hammingDistance(nums[i], nums[j]);
     }
   }
   return totalDistance;
 };
 
-var hammingDistance = function(d1, d2) {
+var hammingDistance = function(x, y) {
+  const padding = dec2Binary(Math.max(x, y)).length;
+  const d1 = dec2Binary(x, padding);
+  const d2 = dec2Binary(y, padding);
   let diff = 0;
   for (let i = 0; i < d1.length; i++) {
     if (d1[i] !== d2[i]) diff++;
   }
   return diff;
-}
+};
 
-var dec2Binary = function(num, length) {
-  const binary = num.toString(2);
+var dec2Binary = function(dec, length) {
+  const binary = dec.toString(2);
   if (length > binary.length) {
     const padding = length - binary.length;
-    return '0'.repeat(padding) + binary
+    return '0'.repeat(padding) + binary;
   }
   return binary;
 }
