c4ca5d39a4d9564bf14fee68d9be86cde3c117d2
jacklam718 <jacklam718@gmail.com>
2019-05-12 14:35:54 +0800

    Add first bad version challenge

---
+++ binary-search/first-bad-version.js
@@ -0,0 +1,38 @@
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function(isBadVersion) {
+  /**
+   * @param {integer} n Total versions
+   * @return {integer} The first bad version
+   */
+  return function(n) {
+    let left = 0;
+    let right = n;
+    let firstBadVersion = null;
+    while (left <= right) {        
+      const mid = ~~(left + right) / 2;
+      if (isBadVersion(mid) && (mid < firstBadVersion || firstBadVersion === null)) {
+        firstBadVersion = mid;
+      }
+      // to left else to right
+      if (isBadVersion(mid)) {
+        right = mid-1;
+      } else {
+        left = mid+1;
+      }
+    }
+    return firstBadVersion;
+  };
+};
